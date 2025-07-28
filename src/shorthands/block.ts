47888640e8086af7c6b1841ea53a7f3f7ea9c22f
Corno Schraverus <corno@schraverus.com>
2025-07-28 20:58:55 +0200

    .

--- src/shorthands/block.ts
+++ src/shorthands/block.ts
@@ -1,44 +1,46 @@
 import * as pd from 'exupery-core-data'
 
-import * as _resolved from "../generated/interface/schemas/block/unconstrained"
+import * as _out from "../generated/interface/schemas/block/unconstrained"
+
+import * as sh from "exupery-core-data/dist/shorthands/unconstrained"
 
 export namespace b {
 
-    export const simple_line = (line: string): _resolved.Block_Part => ['line', line]
+    export const simple_line = (line: string): _out.Block_Part => ['line', line]
 
-    export const nested_line = (snippets: _resolved.Line_Part[]): _resolved.Block_Part => ['nested line', pd.a(snippets)]
+    export const nested_line = (snippets: _out.Line_Part[]): _out.Block_Part => ['nested line', pd.a(snippets)]
 
-    export const sub = (block_parts: _resolved.Block_Part[]): _resolved.Block_Part => ['sub block', pd.a(block_parts)]
+    export const sub = (block_parts: _out.Block_Part[]): _out.Block_Part => ['sub block', pd.a(block_parts)]
 
-    export const sub_decorated = (block: _resolved.Block): _resolved.Block_Part => ['sub block', block]
+    export const sub_decorated = (block: _out.Block): _out.Block_Part => ['sub block', block]
 
-    export const nothing = (): _resolved.Block_Part => ['nothing', null]
+    export const nothing = (): _out.Block_Part => ['nothing', null]
 
 }
 
 export namespace l {
 
-    export const indent = (lines: _resolved.Block_Part[]): _resolved.Line_Part => ['indent', pd.a(lines)]
+    export const indent = (lines: _out.Block_Part[]): _out.Line_Part => ['indent', pd.a(lines)]
 
-    export const snippet = (snippet: string): _resolved.Line_Part => ['snippet', snippet]
+    export const snippet = (snippet: string): _out.Line_Part => ['snippet', snippet]
 
-    export const sub = (line_parts: _resolved.Line_Part[]): _resolved.Line_Part => ['sub line', pd.a(line_parts)]
+    export const sub = (line_parts: _out.Line_Part[]): _out.Line_Part => ['sub line', pd.a(line_parts)]
 
-    export const sub_decorated = (line: _resolved.Line): _resolved.Line_Part => ['sub line', line]
+    export const sub_decorated = (line: _out.Line): _out.Line_Part => ['sub line', line]
 
-    export const nothing = (): _resolved.Line_Part => ['nothing', null]
+    export const nothing = (): _out.Line_Part => ['nothing', null]
 
 }
 
 export namespace d {
 
     export const file = (
-        block_parts: _resolved.Block_Part[]
-    ): _resolved.Directory.D => ['file', pd.a(block_parts)]
+        block_parts: _out.Block_Part[]
+    ): _out.Directory.D => ['file', pd.a(block_parts)]
 
     export const directory = (
-        children: { [key: string]: _resolved.Directory.D },
-    ): _resolved.Directory.D => ['directory', pd.d(children)]
+        children: sh.Raw_Or_Normal_Dictionary<_out.Directory.D>,
+    ): _out.Directory.D => ['directory', sh.wrap_dictionary(children)]
 }
 
-export const block = (block_parts: _resolved.Block_Part[]): _resolved.Block => pd.a(block_parts)
+export const block = (block_parts: _out.Block_Part[]): _out.Block => pd.a(block_parts)
