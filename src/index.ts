94091e03c3c08c79031671c3a794abdc3bc58a2a
Corno Schraverus <corno@schraverus.com>
2025-09-29 09:11:50 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-export * as write_to_disk from "./commands/write_to_file_system"
-
-export * as serialize_block from "./serialize/block"
-export * as serialize_semi_lines from "./serialize/semi_lines"
-
-export * as shorthands_block from "./shorthands/block"
-export * as shorthands_lines from "./shorthands/lines"
-
-export * as transformations_block_2_lines from "./transformations/block/lines"
-export * as transformations_semi_lines_2_lines from "./transformations/semi_lines/lines"
-export * as transformations_block_2_semi_lines from "./transformations/block/semi_lines"
+const dont_import_the_root_of_this_package_but_the_specific_file_or_files_needed = 42
