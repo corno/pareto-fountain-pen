fb3c07719a47695a0fcae86f98316954f7588fde
Corno Schraverus <corno@schraverus.com>
2025-12-22 10:40:45 +0100

    ...

--- pub/src/implementation/generated/pareto/schemas/block/deserialize.ts
+++ pub/src/implementation/generated/pareto/schemas/block/deserialize.ts
@@ -5,9 +5,9 @@ import * as _i_signatures from "../../../../../interface/generated/pareto/schema
 import * as _i_out from "../../../../../interface/generated/pareto/schemas/block/data_types/target"
 
 
-export const Block_Part: _i_signatures._T_Block_Part = ($, $p) => _pd.implement_me()
-export const Block: _i_signatures._T_Block = ($, $p) => _pd.implement_me()
-export const Group_Part: _i_signatures._T_Group_Part = ($, $p) => _pd.implement_me()
-export const Group: _i_signatures._T_Group = ($, $p) => _pd.implement_me()
-export const Node: _i_signatures._T_Node = ($, $p) => _pd.implement_me()
-export const Directory: _i_signatures._T_Directory = ($, $p) => _pd.implement_me()
+export const Block_Part: _i_signatures._T_Block_Part = ($, $p) => _pd.implement_me("yy")
+export const Block: _i_signatures._T_Block = ($, $p) => _pd.implement_me("yy")
+export const Group_Part: _i_signatures._T_Group_Part = ($, $p) => _pd.implement_me("yy")
+export const Group: _i_signatures._T_Group = ($, $p) => _pd.implement_me("yy")
+export const Node: _i_signatures._T_Node = ($, $p) => _pd.implement_me("yy")
+export const Directory: _i_signatures._T_Directory = ($, $p) => _pd.implement_me("yy")
--- pub/src/implementation/generated/pareto/schemas/semi_lines/deserialize.ts
+++ pub/src/implementation/generated/pareto/schemas/semi_lines/deserialize.ts
@@ -5,5 +5,5 @@ import * as _i_signatures from "../../../../../interface/generated/pareto/schema
 import * as _i_out from "../../../../../interface/generated/pareto/schemas/semi_lines/data_types/target"
 
 
-export const Lines: _i_signatures._T_Lines = ($, $p) => _pd.implement_me()
-export const Directory: _i_signatures._T_Directory = ($, $p) => _pd.implement_me()
+export const Lines: _i_signatures._T_Lines = ($, $p) => _pd.implement_me("yy")
+export const Directory: _i_signatures._T_Directory = ($, $p) => _pd.implement_me("yy")
