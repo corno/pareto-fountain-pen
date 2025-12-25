53e2c0a3dff2b3665e396960af3ff0533fda0962
Corno Schraverus <corno@schraverus.com>
2025-12-26 00:15:21 +0100

    ...

--- pub/src/implementation/serializers/schemas/semi_lines.ts
+++ pub/src/implementation/serializers/schemas/semi_lines.ts
@@ -5,7 +5,7 @@ import * as t_fountain_pen_semi_lines_to_lines from "../../transformers/schemas/
 import * as d_in from "../../../interface/generated/pareto/schemas/semi_lines/data_types/source"
 import * as d_out from "../../../interface/generated/pareto/schemas/text/data_types/target"
 
-import { $$ as op_join_list_of_texts } from "pareto-standard-operations/dist/implementation/operations/pure/text/join_list_of_texts"
+import { $$ as s_join_list_of_texts } from "pareto-standard-operations/dist/implementation/serializers/schemas/list_of_texts"
 
 export const Lines = (
     $: d_in.Lines,
@@ -13,7 +13,7 @@ export const Lines = (
         'indentation': string
         'newline': string
     }
-): string => op_join_list_of_texts(
+): string => s_join_list_of_texts(
     t_fountain_pen_semi_lines_to_lines.Lines(
         $,
         {
