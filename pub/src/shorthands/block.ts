ae486e5705262ec82a2e085cd53dbd16cbe967d2
Corno Schraverus <corno@schraverus.com>
2026-01-02 17:45:49 +0100

    ...

--- pub/src/shorthands/block.ts
+++ pub/src/shorthands/block.ts
@@ -1,50 +1,50 @@
 import * as _pi from 'pareto-core-interface'
 import * as sh from 'pareto-core-shorthands/dist/unconstrained'
 
-import * as _target from "../interface/generated/pareto/schemas/block/data_types/target"
+import * as d_target from "../interface/generated/pareto/schemas/block/data_types/target"
 
 
 export namespace g {
 
-    export const simple_block = (block: string): _target.Group_Part => ['block', block]
+    export const simple_block = (block: string): d_target.Group_Part => ['block', block]
 
-    export const nested_block = (snippets: sh.Raw_Or_Normal_List<_target.Block.L>): _target.Group_Part => ['nested block', sh.wrap_list(snippets)]
+    export const nested_block = (snippets: sh.Raw_Or_Normal_List<d_target.Block.L>): d_target.Group_Part => ['nested block', sh.wrap_list(snippets)]
 
-    export const sub = (group_parts: sh.Raw_Or_Normal_List<_target.Group.L>): _target.Group_Part => ['sub group', sh.wrap_list(group_parts)]
+    export const sub = (group_parts: sh.Raw_Or_Normal_List<d_target.Group.L>): d_target.Group_Part => ['sub group', sh.wrap_list(group_parts)]
 
-    export const nothing = (): _target.Group_Part => ['nothing', null]
+    export const nothing = (): d_target.Group_Part => ['nothing', null]
 
-    export const optional = (Group_Part: _pi.Optional_Value<_target.Group.L>): _target.Group_Part => ['optional', Group_Part]
+    export const optional = (Group_Part: _pi.Optional_Value<d_target.Group.L>): d_target.Group_Part => ['optional', Group_Part]
 
 }
 
 export namespace b {
 
-    export const indent = (blocks: sh.Raw_Or_Normal_List<_target.Group.L>): _target.Block_Part => ['indent', sh.wrap_list(blocks)]
+    export const indent = (blocks: sh.Raw_Or_Normal_List<d_target.Group.L>): d_target.Block_Part => ['indent', sh.wrap_list(blocks)]
 
-    export const snippet = (snippet: string): _target.Block_Part => ['snippet', snippet]
+    export const snippet = (snippet: string): d_target.Block_Part => ['snippet', snippet]
 
-    export const sub = (block_parts: sh.Raw_Or_Normal_List<_target.Block.L>): _target.Block_Part => ['sub block', sh.wrap_list(block_parts)]
+    export const sub = (block_parts: sh.Raw_Or_Normal_List<d_target.Block.L>): d_target.Block_Part => ['sub block', sh.wrap_list(block_parts)]
 
-    export const nothing = (): _target.Block_Part => ['nothing', null]
+    export const nothing = (): d_target.Block_Part => ['nothing', null]
 
-    export const optional = (block_part: _pi.Optional_Value<_target.Block.L>): _target.Block_Part => ['optional', block_part]
+    export const optional = (block_part: _pi.Optional_Value<d_target.Block.L>): d_target.Block_Part => ['optional', block_part]
 
 }
 
 export namespace n {
 
     export const file = (
-        block: _target.Group
-    ): _target.Node => ['file', block]
+        block: d_target.Group
+    ): d_target.Node => ['file', block]
 
     export const directory = (
-        children: sh.Raw_Or_Normal_Dictionary<_target.Node>,
-    ): _target.Node => ['directory', sh.wrap_dictionary(children)]
+        children: sh.Raw_Or_Normal_Dictionary<d_target.Node>,
+    ): d_target.Node => ['directory', sh.wrap_dictionary(children)]
 }
 
 export const directory = (
-    children: sh.Raw_Or_Normal_Dictionary<_target.Node>,
-): _target.Directory => sh.wrap_dictionary(children)
+    children: sh.Raw_Or_Normal_Dictionary<d_target.Node>,
+): d_target.Directory => sh.wrap_dictionary(children)
 
-export const group = (Group_Parts: sh.Raw_Or_Normal_List<_target.Group.L>): _target.Group => sh.wrap_list(Group_Parts)
+export const group = (Group_Parts: sh.Raw_Or_Normal_List<d_target.Group.L>): d_target.Group => sh.wrap_list(Group_Parts)
--- pub/src/shorthands/lines.ts
+++ pub/src/shorthands/lines.ts
@@ -1,14 +1,14 @@
 import { Raw_Or_Normal_Dictionary, wrap_dictionary } from 'pareto-core-shorthands/dist/unconstrained'
 
-import * as _target from "../interface/generated/pareto/schemas/lines/data_types/target"
+import * as d_target from "../interface/generated/pareto/schemas/lines/data_types/target"
 
 export namespace d {
 
     export const file = (
-        lines: _target.Lines
-    ): _target.Directory.D => ['file', lines]
+        lines: d_target.Lines
+    ): d_target.Directory.D => ['file', lines]
 
     export const directory = (
-        children: Raw_Or_Normal_Dictionary<_target.Directory.D>,
-    ): _target.Directory.D => ['directory', wrap_dictionary(children)]
+        children: Raw_Or_Normal_Dictionary<d_target.Directory.D>,
+    ): d_target.Directory.D => ['directory', wrap_dictionary(children)]
 }
