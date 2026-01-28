f9022d8fcf7eb326bba0e6be835d1c19e2eaf4a5
Corno Schraverus <corno@schraverus.com>
2026-01-28 18:19:44 +0100

    pdt: ...

--- pub/src/implementation/generated/liana/schemas/text/migrate_boilerplate.ts
+++ pub/src/implementation/generated/liana/schemas/text/migrate_boilerplate.ts
@@ -4,14 +4,31 @@ import * as _p from "pareto-core/dist/transformer"
 import * as t_signatures from "../../../../../interface/generated/liana/schemas/text/migrate_boilerplate"
 
 import * as t_out from "../../../../../interface/generated/liana/schemas/text/data"
-export const Lines: t_signatures.Lines = ($,) => $
-export const Directory: t_signatures.Directory = ($,) => $.__d_map(($,id,) => _p.decide.state($, ($,): t_out.Directory.D => {
-    switch ($[0]) {
-        case 'file':
-            return _p.ss($, ($,) => ['file', Lines($)])
-        case 'directory':
-            return _p.ss($, ($,) => ['directory', Directory($)])
-        default:
-            return _p.au($[0])
-    }
-}))
+export const Lines: t_signatures.Lines = ($) => $
+export const Directory: t_signatures.Directory = ($) => $.__d_map(
+    ($,id) => _p.decide.state(
+        $, 
+        ($): t_out.Directory.D => {
+            switch ($[0]) {
+                case 'file':
+                    return _p.ss(
+                        $, 
+                        ($) => ['file', Lines(
+                            $
+                        )]
+                    )
+                case 'directory':
+                    return _p.ss(
+                        $, 
+                        ($) => ['directory', Directory(
+                            $
+                        )]
+                    )
+                default:
+                    return _p.au(
+                        $[0]
+                    )
+            }
+        }
+    )
+)
