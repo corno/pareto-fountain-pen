a3af34585b271f28f6c2c4b3f5fc0c3c6f697b35
Corno Schraverus <corno@schraverus.com>
2026-07-06 10:03:39 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/transformers/prose/list_of_characters.ts
+++ typescript/lib/src/implementation/manual/transformers/prose/list_of_characters.ts
@@ -3,19 +3,19 @@ import * as p_i from 'pareto-core/interface/transformer'
 
 //data types
 import * as d_in from "../../../../interface/generated/liana/schemas/prose/data.js"
-import * as d_x from "../../../../interface/data/prose_serialize.js"
+import * as d_function from "../../../../interface/data/prose_serialize.js"
 import * as d_out from "../../../../interface/generated/liana/schemas/list_of_characters/data.js"
 
 export namespace interface_ {
     export type Paragraph = p_i.Transformer_With_Parameter<
         d_in.Paragraph,
         d_out.List_of_Characters,
-        d_x.Parameters
+        d_function.Parameters
     >
     export type Phrase = p_i.Transformer_With_Parameter<
         d_in.Phrase,
         d_out.List_of_Characters,
-        d_x.Parameters
+        d_function.Parameters
     >
 }
 
