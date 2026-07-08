b53d941c74c8e6d69a1adc896a73a2070beb68e5
Corno Schraverus <corno@schraverus.com>
2026-07-08 13:44:07 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/prose/text.ts
+++ typescript/lib/src/interface/declarations/transformers/prose/text.ts
@@ -1,5 +1,4 @@
 import type * as p_ from 'pareto-core/interface/transformer'
-import p_text_from_list from 'pareto-core/implementation/transformer/specials/text_from_list'
 
 import type * as d_in from "../../../generated/liana/schemas/prose/data.js"
 import type * as d_function from "../../../data/prose_serialize.js"
--- typescript/lib/src/interface/declarations/transformers/semi_lines/text.ts
+++ typescript/lib/src/interface/declarations/transformers/semi_lines/text.ts
@@ -1,9 +1,5 @@
 import type * as p_ from 'pareto-core/interface/transformer'
 
-import p_list_from_text from 'pareto-core/implementation/refiner/specials/list_from_text'
-
-import * as t_fountain_pen_semi_lines_to_lines from "./lines.js"
-
 import type * as d_in from "../../../generated/liana/schemas/semi_lines/data.js"
 import type * as d_function from "../../../data/semi_lines_serialize.js"
 import type * as d_out from "../../../generated/liana/schemas/list_of_characters/data.js"
