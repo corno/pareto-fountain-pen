e9c3ed1ab5ac410737b816a20fd0a5ff98209ad6
Corno Schraverus <corno@schraverus.com>
2026-07-13 19:13:59 +0200

    pdt: published version 0.4.96

--- typescript/lib/src/api.ts
+++ typescript/lib/src/api.ts
@@ -1,4 +1,12 @@
 import * as interface_ from "./interface/api.js"
 
+import * as ser from "./_implementation/serializers/prose.js"
+
 export const api: interface_.API = {
+
+    'serializers': {
+        'prose': {
+            'Phrase': ser.Phrase,
+        },
+    },
 }
--- typescript/lib/src/interface/api.ts
+++ typescript/lib/src/interface/api.ts
@@ -1,7 +1,14 @@
 import * as p_t from 'pareto-core/interface/transformer'
 import * as p_s from 'pareto-core/implementation/serializer'
 
-
+import * as s_prose from "./schemas/prose.js"
 
 export type API = {
+    'serializers': {
+        'prose': {
+            'Phrase': p_s.Serializer<
+                s_prose.Phrase
+            >
+        },
+    },
 }
