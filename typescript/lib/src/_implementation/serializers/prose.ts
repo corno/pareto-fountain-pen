60c9b72801c288744a22e570c23283ec204bd875
Corno Schraverus <corno@schraverus.com>
2026-07-13 19:10:17 +0200

    pdt: published version 0.4.95

---
+++ typescript/lib/src/_implementation/serializers/prose.ts
@@ -0,0 +1,7 @@
+import * as p_ from 'pareto-core/implementation/serializer'
+
+import * as s_in from "../../interface/schemas/prose.js"
+
+export const Phrase: p_.Serializer<
+    s_in.Phrase
+> = ($) => $
