5092e26a42131edd7f8f13bcf524832666d75a4b
Corno Schraverus <corno@schraverus.com>
2026-07-15 08:54:37 +0200

    pdt: ...

---
+++ typescript/lib/src/shorthands/prose_simple_with_rich/deprecated.ts
@@ -0,0 +1,42 @@
+import * as p_ from 'pareto-core-shorthands/unconstrained_deprecated'
+import * as p_di from 'pareto-core/interface/schema'
+import p_text_from_list from 'pareto-core/implementation/transformer/specials/text_from_list'
+import * as p_s from 'pareto-core/implementation/serializer'
+
+import type * as s_target from "pareto-core/temp/fountain_pen/prose"
+
+
+
+export namespace ph {
+    export const composed = (
+        phrases: p_.Raw_Or_Normal_List<s_target.Phrase>
+    ): s_target.Phrase => ['composed', p_.list(phrases)]
+
+    export const literal = (
+        value: string
+    ): s_target.Phrase => ['value', ['text', value]]
+
+    export const list_of_characters = (
+        value: p_di.List<number>
+    ): s_target.Phrase => ['value', ['text', p_text_from_list(
+        p_.list(value),
+        ($) => $,
+    )]]
+
+
+    export const rich_phrase = (
+        items: p_.Raw_Or_Normal_List<s_target.Phrase>,
+        if_empty: null | s_target.Phrase,
+        before: null | s_target.Phrase,
+        separator: s_target.Phrase,
+        after: null | s_target.Phrase,
+    ): s_target.Phrase => ['rich phrase', {
+        'items': p_.list(items),
+        'if empty': if_empty ? if_empty : ['nothing', null],
+        'if not empty': {
+            'before': before ? before : ['nothing', null],
+            'separator': separator,
+            'after': after ? after : ['nothing', null],
+        },
+    }]
+}
